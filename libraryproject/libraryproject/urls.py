d36a6478579ef677c968621c331b182fd72fe8e0
Saleh <431107908@qu.edu.sa>
2026-04-27 11:48:29 +0300

    fix: fix page not found for all lab7 and lab8 tasks

--- libraryproject/libraryproject/urls.py
+++ libraryproject/libraryproject/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include("apps.bookmodule.urls")),
     path('users/', include("apps.usermodule.urls"))
+    ,path('', lambda request: redirect('books/'))
 ]
